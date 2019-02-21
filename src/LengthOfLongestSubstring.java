5c6856bafa3f28efb1ab739df858868798753624
dengdairiluo <1020447548@qq.com>
2019-02-21 19:34:44 +0800

    LengthOfLongestSubstring

---
+++ src/LengthOfLongestSubstring.java
@@ -0,0 +1,90 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author lujiang
+ * @date 2019-02-21 18:33
+ */
+public class LengthOfLongestSubstring {
+
+    public int lengthOfLongestSubstring1(String s) {
+        int n = s.length();
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (isDifferent(s, i, j)) {
+                    result = Math.max(result, j - i);
+                }
+            }
+        }
+        return result;
+    }
+
+    public int lengthOfLongestSubstring2(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int result = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                result = Math.max(result, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * todo 没看懂
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring3(String s) {
+        int n = s.length(), result = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            result = Math.max(result, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return result;
+    }
+
+    /**
+     * todo 没看懂
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring4(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128]; // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1;
+        }
+        return ans;
+    }
+
+    private boolean isDifferent(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            char c = s.charAt(i);
+            if (set.contains(c)) {
+                return false;
+            } else {
+                set.add(c);
+            }
+        }
+        return true;
+    }
+}
